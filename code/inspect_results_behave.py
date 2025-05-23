283730509614afde6fcb44bc517d673e0050cea9
Matthew Crossley <mq20185996@Matthews-MacBook-Air.local>
2025-05-23 15:57:55 +1000

    some loose code

--- code/inspect_results_behave.py
+++ code/inspect_results_behave.py
@@ -13,3 +13,8 @@ dd2 = dd.loc[dd['exp'] == 'Exp 2'].copy()
 
 report_exp_1(dd1)
 report_exp_2(dd2)
+
+d3 = load_data_exp_3()
+dd3 = d3.groupby(['exp', 'cnd', 'sub'])[['t2c', 'nps']].mean().reset_index()
+print(dd3.groupby(['exp', 'cnd'])['sub'].nunique())
+report_exp_3(dd3)
